1bdc0dc65ca7164112813082548f326122a33c8c
HeadlessJohn <o_ayjh@naver.com>
2024-05-14 19:37:03 +0900

    Fix: untracked files remove

--- sources_1/new/fan_project.v
+++ sources_1/new/fan_project.v
@@ -2,23 +2,35 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module project_1(
-    input clk, reset_p,
-    input [2:0] btn,
-    inout dht11_data,
-    output [7:0] led_bar,
-    output pwm, led,
-    output [3:0] com,
-    output [7:0] seg_7,
-    output buz_clk,
-    output sda, scl    );
+    input        clk, 
+    input        reset_p,
+    input  [2:0] btn,
+    inout        dht11_data,
+
+    output [7:0] led_bar, //for debug
+    output       pwm, 
+    output       led,
+    output [3:0] com,     //for debug
+    output [7:0] seg_7,   //for debug
+    output       buz_clk,
+    output       sda,
+    output       scl    );
+
+    localparam SYS_FREQ              = 125;      // 125MHz
+    localparam BTN_HOLD_TIME         = 700_000;  // 700ms
+    localparam BTN_DOUBLE_TAP_TIME   = 100_000;  // 100ms
     
-    wire [7:0] fan_led, timer_led;
+    wire        fan_en, run_e;
+    wire [7:0]  fan_led, timer_led;
     wire [19:0] cur_time;
-    assign led_bar = {fan_led[4:1], timer_led[3:0]};
-    wire fan_en, run_e;
-    wire [7:0] fan_speed;
-    wire [3:0] fan_timer_state;
-    reg buz_on;
+    wire [7:0]  fan_speed;
+    wire [3:0]  fan_timer_state;
+
+    assign      fan_en = timeout_pedge ? 0 : 1;
+    assign      led_bar = {fan_led[4:1], timer_led[3:0]};
+
+    reg         buz_on;
+
     always @(posedge clk, posedge reset_p)begin
         if(reset_p)begin
             buz_on <= 0;
@@ -47,29 +59,29 @@ module project_1(
                   .time_s_1       (cur_time[ 3: 0])    );
     
     wire [1:0] btn_single, btn_double, btn_long;
-    btn_double_long #(700_000, 100_000) btn_fan_cntr (.clk           (clk), 
-                                                      .reset_p       (reset_p), 
-                                                      .btn           (btn[0]),
-                                                      .single        (btn_single[0]), 
-                                                      .double        (btn_double[0]), 
-                                                      .long          (btn_long[0])    );
+    btn_double_long #(BTN_HOLD_TIME, BTN_DOUBLE_TAP_TIME) btn_fan_cntr (.clk     (clk), 
+                                                                        .reset_p (reset_p), 
+                                                                        .btn     (btn[0]),
+                                                                        .single  (btn_single[0]), 
+                                                                        .double  (btn_double[0]), 
+                                                                        .long    (btn_long[0])    );
+                  
+    btn_double_long #(BTN_HOLD_TIME, BTN_DOUBLE_TAP_TIME) btn_led_cntr (.clk     (clk), 
+                                                                        .reset_p (reset_p), 
+                                                                        .btn     (btn[1]),
+                                                                        .single  (btn_single[1]), 
+                                                                        .double  (btn_double[1]), 
+                                                                        .long    (btn_long[1])    );
 
-    btn_double_long #(700_000, 100_000) btn_led_cntr (.clk           (clk), 
-                                                      .reset_p       (reset_p), 
-                                                      .btn           (btn[1]),
-                                                      .single        (btn_single[1]), 
-                                                      .double        (btn_double[1]), 
-                                                      .long          (btn_long[1])    );
-
-    fan_controller #(125, 12) (.clk      (clk), 
-                               .reset_p  (reset_p), 
-                               .btn      (btn_single[0]), 
-                               .btn_back (btn_double[0]),
-                               .set_idle (btn_long[0]),
-                               .fan_en   (fan_en), 
-                               .state    (fan_speed), 
-                               .pwm      (pwm), 
-                               .run_e    (run_e));
+    fan_controller #(SYS_FREQ, 12) (.clk      (clk), 
+                                    .reset_p  (reset_p), 
+                                    .btn      (btn_single[0]), 
+                                    .btn_back (btn_double[0]),
+                                    .set_idle (btn_long[0]),
+                                    .fan_en   (fan_en), 
+                                    .state    (fan_speed), 
+                                    .pwm      (pwm), 
+                                    .run_e    (run_e));
 
     led_controller led_cntr(clk, reset_p, btn_single[1], btn_long[1], led);
     
@@ -77,7 +89,6 @@ module project_1(
 
     buz_top buzz(.clk (clk), .reset_p (reset_p), .buz_on (buz_on), .buz_clk (buz_clk));
    
-    assign fan_en = timeout_pedge ? 0 : 1;
     
     fnd_4_digit_cntr fnd(.clk(clk), .reset_p(reset_p), .value(cur_time[15:0]), .segment_data_ca(seg_7), .com_sel(com));
     
